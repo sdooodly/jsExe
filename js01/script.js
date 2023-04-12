55629490e1a0bf9373779480876d306b55cd0a49
Gayathri.Sujatha <Gayathri.Sujatha@arup.com>
2023-04-12 16:01:50 +0530

    Update with js functions

---
+++ js01/script.js
@@ -0,0 +1,18 @@
+//check these functions in console.log
+
+const add7 = (number) => {
+ return (number + 7);
+}
+
+const multiply = (number01, number02) => {
+ return (number01 * number02);
+}
+
+const capitalize = (text) => {
+ return (text.charAt(0).toUpperCase() + text.slice(1))
+}
+
+const lastLetter = (text) => {
+ return text.slice(text.length - 1);
+}
+
